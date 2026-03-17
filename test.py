83e6c7aac45308c805f31421b588c12e2c180d4b
Александр Чернышев <sanyachernishev@mail.ru>
2026-03-17 22:04:14 +0300

    kek

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-import sys
-import os
-x=1+1
-def hello():
-    print("hello")
+from math import *
+import itertools
+
+
+def CalculateSquareRoot(Number):
+    return sqrt(Number)
+
+
+def append_item(item, l=[]):
+    l.append(item)
+    return l
+while True:
+    try:
+        your_number = float(input("Enter your number: "))
+        print("Square root is:", CalculateSquareRoot(your_number))
+        break
+    except:
+        pass
